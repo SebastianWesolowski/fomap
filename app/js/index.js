5a476b8b66d2912e751da0668d6d979465ddea0a
SebastianWesolowski <sebastian.wesolowski.sw@gmail.com>
2019-08-05 22:08:24 +0200

    add object nutritional,ingredients

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,11 @@
 console.clear();
-console.log('test');
+
+
+// const ingredients = require('./ingredients.js');
+const nutritional = require('./nutritional.js');
+
+
+const nutritionalItems = nutritional.filter(item => item.value > 1);
+
+console.clear();
+console.log(nutritionalItems);
---
+++ app/js/ingredients.js
@@ -0,0 +1,7 @@
+const ingredients = [
+  { Id: 0, NameEn: 'Carrot', NutritionalIngredientsId: [0,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20] },
+  { Id: 1, NameEn: 'Apple', NutritionalIngredientsId: [0,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20] },
+  { Id: 2, NameEn: 'Cheese', NutritionalIngredientsId: [0,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20] },
+];
+
+module.exports = ingredients;
