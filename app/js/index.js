17b685f9c9997f361943e0d03c7a043cd8653f20
Sebastian Wesołowski <sebastian@artnova.com.pl>
2019-08-06 17:46:38 +0200

    change json

--- app/js/index.js
+++ app/js/index.js
@@ -1,22 +1,23 @@
 const ingredients = require('./ingredients.js');
 const nutritional = require('./nutritional.js');
 
-const ingredientsList = ingredients.map(({ Id, NameEn, NutritionalIngredientsId }) => {
-  const arrayElet = [];
-  const arrayElement = NutritionalIngredientsId;
+const ingredientsList = ingredients.map(
+  ({ Id, NameEn, NutritionalIngredientsId }) => {
+    const arrayElet = [];
+    const arrayElement = NutritionalIngredientsId;
 
-  arrayElement.forEach((element) => {
-    const getNutritional = nutritional.find(({ Id }) => Id === element);
+    arrayElement.forEach(element => {
+      const getNutritional = nutritional.find(({ Id }) => Id === element);
 
-    return arrayElet.push(getNutritional);
-  });
+      return arrayElet.push(getNutritional);
+    });
 
-  const arrAll = arrayElet.map(({ Id, nameEn, Value }) => {
-    return `${Id} | ${nameEn} |  ${Value}`
-  });
+    const arrAll = arrayElet.map(({ Id, nameEn, Value, unit }) => {
+      return `${Id} | ${nameEn} |  ${Value} ${unit}`;
+    });
 
-
-  return `${Id} | ${NameEn} |  ${arrAll}`
-})
-
+    return `${Id} | ${NameEn} |  ${arrAll}`;
+  }
+);
+console.clear;
 console.log(ingredientsList);
