1b488bc55c26bc50f516233bb46a0ab969420712
SebastianWesolowski <sebastian.wesolowski.sw@gmail.com>
2019-08-14 20:58:36 +0200

    change root app folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
   firebaseConfig = {
-    apiKey: "AIzaSyB8O5OB1yn37L2Aj50KHf2-z6ZCNDM0-sk",
-    authDomain: "fomap-f430b.firebaseapp.com",
-    databaseURL: "https://fomap-f430b.firebaseio.com",
-    projectId: "fomap-f430b",
-    storageBucket: "fomap-f430b.appspot.com",
-    messagingSenderId: "812148066901",
-    appId: "1:812148066901:web:887b39df646a8bb9"
-  };
+    apiKey: 'AIzaSyB8O5OB1yn37L2Aj50KHf2-z6ZCNDM0-sk',
+    authDomain: 'fomap-f430b.firebaseapp.com',
+    databaseURL: 'https://fomap-f430b.firebaseio.com',
+    projectId: 'fomap-f430b',
+    storageBucket: 'fomap-f430b.appspot.com',
+    messagingSenderId: '812148066901',
+    appId: '1:812148066901:web:887b39df646a8bb9',
+  },
 };
 
 /*
