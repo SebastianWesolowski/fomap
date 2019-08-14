c64ded370b343c146c1b9aae75a1abe4def7593a
SebastianWesolowski <sebastian.wesolowski.sw@gmail.com>
2019-08-14 20:30:38 +0200

    add env for firebase Config

--- app/aplication/DietApp/src/environments/environment.ts
+++ app/aplication/DietApp/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false
+  firebaseConfig = {
+    apiKey: "AIzaSyB8O5OB1yn37L2Aj50KHf2-z6ZCNDM0-sk",
+    authDomain: "fomap-f430b.firebaseapp.com",
+    databaseURL: "https://fomap-f430b.firebaseio.com",
+    projectId: "fomap-f430b",
+    storageBucket: "fomap-f430b.appspot.com",
+    messagingSenderId: "812148066901",
+    appId: "1:812148066901:web:887b39df646a8bb9"
+  };
 };
 
 /*
